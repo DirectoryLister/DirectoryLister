4678b4aa91426744c6154588d38162b57979b37d
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2012-02-01 11:26:27 -0700

    Reverted to divs for file listing and restyled them to match Bootstrap styles

--- index.php
+++ index.php
@@ -6,11 +6,11 @@
     <link rel="shortcut icon" href="resources/img/icons/folder.png" />
     
     <link rel="stylesheet" type="text/css" href="resources/css/bootstrap.min.css" />
-    <link rel="stylesheet" type="text/css" href="resources/css/style.css" />
+    <link rel="stylesheet" type="text/css" href="resources/css/directorylister.css" />
     
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
     <script type="text/javascript" src="resources/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="resources/js/custom.js"></script>
+    <script type="text/javascript" src="resources/js/directorylister.js"></script>
     
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 </head>
@@ -35,29 +35,23 @@
         </ul>
     </div>
     
-    <table id="directoryListerTable" class="table table-striped">
-        <thead>
-            <tr>
-                <th>File</th>
-                <th width="50">Size</th>
-                <th width="120">Last Modified</th>
-            </tr>
-        </thead>
-        
-        <tbody>
-            <?php $x = 1; foreach($lister->listDirectory() as $name => $fileInfo): ?>
-                <tr>
-                    <td class="fileName">
-                        <a href="<?php if(is_dir($fileInfo['file_path'])) { echo '?dir=' . $fileInfo['file_path']; } else { echo $fileInfo['file_path']; } ?>">
-                            <?php echo $name; ?>
-                        </a>
-                    </td>
-                    <td class="fileSize"><?php echo $fileInfo['file_size']; ?></td>
-                    <td class="fileTime"><?php echo $fileInfo['mod_time']; ?></td>
-                </tr>
-            <?php $x++; endforeach; ?>
-        </tbody>
-    </table>
+    <div id="header" class="clearfix">
+        <span class="fileName">File</span>
+        <span class="fileSize">Size</span>
+        <span class="fileModTime">Last Modified</span>
+    </div>
+    
+    <ul id="directoryListing">
+    <?php $x = 1; foreach($lister->listDirectory() as $name => $fileInfo): ?>
+        <li class="<?php echo $x %2 == 0 ? 'even' : 'odd'; ?>">
+            <a href="<?php if(is_dir($fileInfo['file_path'])) { echo '?dir=' . $fileInfo['file_path']; } else { echo $fileInfo['file_path']; } ?>" class="clearfix">
+                <span class="fileName" style="background: transparent url(resources/img/icons/<?php echo $fileInfo['icon']; ?>) no-repeat left center;"><?php echo $name; ?></span>
+                <span class="fileSize"><?php echo $fileInfo['file_size']; ?></span>
+                <span class="fileModTime"><?php echo $fileInfo['mod_time']; ?></span>
+            </a>
+        </li>
+    <?php $x++; endforeach; ?>
+    </ul>
 
     <hr/>
 

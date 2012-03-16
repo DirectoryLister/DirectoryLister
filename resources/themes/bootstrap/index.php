5945a1bff2d3df0fc3ac662af56fa0052ac30fa3
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2012-03-16 09:16:27 -0700

    Added bootstrap.min.js because it's required

--- resources/themes/bootstrap/index.php
+++ resources/themes/bootstrap/index.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" type="text/css" href="<?php echo THEMEPATH; ?>/style.css" />
     
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+    <script type="text/javascript" src="<?php echo THEMEPATH; ?>/bootstrap.min.js"></script>
     <script type="text/javascript" src="<?php echo THEMEPATH; ?>/directorylister.js"></script>
     
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />

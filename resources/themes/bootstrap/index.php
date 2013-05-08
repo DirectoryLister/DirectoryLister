7583fa712303cb1c3906e2f0f0cb0013770efe46
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2013-05-08 08:23:41 -0700

    Updated theme jQuery call to v1.9.1 and modified URL to be protocol-agnostic

--- resources/themes/bootstrap/index.php
+++ resources/themes/bootstrap/index.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" type="text/css" href="<?php echo THEMEPATH; ?>/bootstrap.min.css" />
     <link rel="stylesheet" type="text/css" href="<?php echo THEMEPATH; ?>/style.css" />
 
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script type="text/javascript" src="<?php echo THEMEPATH; ?>/bootstrap.min.js"></script>
     <script type="text/javascript" src="<?php echo THEMEPATH; ?>/directorylister.js"></script>
 

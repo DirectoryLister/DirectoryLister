7c7d4348aba981062a56687e1fbd6304d53ae950
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2013-10-09 10:27:43 -0700

    Re-added show/hide top-link javascript

--- resources/themes/bootstrap/js/directorylister.js
+++ resources/themes/bootstrap/js/directorylister.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
 
+    // Get page-content original position
+    var contentTop = $('#page-content').offset().top;
+
+    // Show/hide top link on page load
+    showHideTopLink(contentTop);
+
+    // Show/hide top link on scroll
+    $(window).scroll(function() {
+        showHideTopLink(contentTop);
+    });
+
     // Scroll page on click action
     $('#page-top-link').click(function() {
         $('html, body').animate({ scrollTop: 0 }, 'fast');
@@ -41,3 +52,11 @@ $(document).ready(function() {
     });
 
 });
+
+function showHideTopLink(elTop) {
+    if($('#page-navbar').offset().top + $('#page-navbar').height() >= elTop) {
+        $('#page-top-nav').show();
+    } else {
+        $('#page-top-nav').hide();
+    }
+}

31b2514c4eccfd61d5fabc43ecbaa36a1b909aa1
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2014-02-03 20:38:15 -0700

    Possible fix to file sizes getting cut off

--- resources/themes/bootstrap/index.php
+++ resources/themes/bootstrap/index.php
@@ -67,8 +67,8 @@
 
             <div id="directory-list-header">
                 <div class="row">
-                    <div class="col-md-8 col-sm-6 col-xs-5">File</div>
-                    <div class="col-md-1 col-sm-2 col-xs-2 text-right">Size</div>
+                    <div class="col-md-7 col-sm-6 col-xs-5">File</div>
+                    <div class="col-md-2 col-sm-2 col-xs-2 text-right">Size</div>
                     <div class="col-md-3 col-sm-4 col-xs-5 text-right">Last Modified</div>
                 </div>
             </div>
@@ -81,12 +81,12 @@
 
 
                             <div class="row">
-                                <span class="file-name col-md-8 col-sm-6 col-xs-5">
+                                <span class="file-name col-md-7 col-sm-6 col-xs-5">
                                     <i class="fa <?php echo $fileInfo['icon_class']; ?> fa-fw"></i>
                                     <?php echo $name; ?>
                                 </span>
 
-                                <span class="file-size col-md-1 col-sm-2 col-xs-2 text-right">
+                                <span class="file-size col-md-2 col-sm-2 col-xs-2 text-right">
                                     <?php echo $fileInfo['file_size']; ?>
                                 </span>
 

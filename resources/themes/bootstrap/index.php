d5c961861d1944a3ce1d9d9d55e5d7f1edd8e98a
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2014-12-11 22:27:13 -0700

    Updated jQuery to v2.1.1 in Bootstrap theme

--- resources/themes/bootstrap/index.php
+++ resources/themes/bootstrap/index.php
@@ -13,7 +13,7 @@
         <link rel="stylesheet" type="text/css" href="<?php echo THEMEPATH; ?>/css/style.css">
 
         <!-- SCRIPTS -->
-        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
         <script type="text/javascript" src="<?php echo THEMEPATH; ?>/js/bootstrap.min.js"></script>
         <script type="text/javascript" src="<?php echo THEMEPATH; ?>/js/directorylister.js"></script>
 

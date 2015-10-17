d341687193ff3cce0cd72d3a9777c26b9a946f68
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2015-10-16 23:36:09 -0700

    Bumped version to 2.6.1

--- resources/DirectoryLister.php
+++ resources/DirectoryLister.php
@@ -16,7 +16,7 @@
 class DirectoryLister {
 
     // Define application version
-    const VERSION = '2.6.0';
+    const VERSION = '2.6.1';
 
     // Reserve some variables
     protected $_themeName     = null;

d0cf859f83dadd4a9f7290a5277b3c20db8e95c7
OstenTV <info@ostentv.dk>
2018-04-25 22:02:49 +0200

    Added a bunch of file types.
    Added a bunch of different MS Office file types.
    Added more script files.
    Added more text files.
    Added another video file.
    Fixed .bat both being an Executable and a Script.

--- resources/fileTypes.php
+++ resources/fileTypes.php
@@ -47,16 +47,53 @@ return array(
 
     // Documents
     'csv'   => 'fa-file-text',
-    'doc'   => 'fa-file-text',
-    'docx'  => 'fa-file-text',
     'odt'   => 'fa-file-text',
     'pdf'   => 'fa-file-text',
-    'xls'   => 'fa-file-text',
-    'xlsx'  => 'fa-file-text',
+	
+	// Word Documents
+	'doc'   => 'fa-file-word',
+	'dot'   => 'fa-file-word',
+	'wbk'   => 'fa-file-word',
+	'docx'  => 'fa-file-word',
+	'docm'  => 'fa-file-word',
+	'dotx'  => 'fa-file-word',
+	'dotm'  => 'fa-file-word',
+	'docb'  => 'fa-file-word',
+	
+	// Excel Documents
+	'xls'   => 'fa-file-excel',
+	'xlt'   => 'fa-file-excel',
+	'xlm'   => 'fa-file-excel',
+	'xlsx'  => 'fa-file-excel',
+	'xlsm'  => 'fa-file-excel',
+	'xltx'  => 'fa-file-excel',
+	'xltm'  => 'fa-file-excel',
+	'xlsb'  => 'fa-file-excel',
+	'xla'   => 'fa-file-excel',
+	'xlam'  => 'fa-file-excel',
+	'xll'   => 'fa-file-excel',
+	'xlw'   => 'fa-file-excel',
+	
+	// PowerPoint Documents
+	'ppt'   => 'fa-file-powerpoint',
+	'pot'   => 'fa-file-powerpoint',
+	'pps'   => 'fa-file-powerpoint',
+	'pptx'  => 'fa-file-powerpoint',
+	'pptm'  => 'fa-file-powerpoint',
+	'potx'  => 'fa-file-powerpoint',
+	'potm'  => 'fa-file-powerpoint',
+	'ppam'  => 'fa-file-powerpoint',
+	'ppsx'  => 'fa-file-powerpoint',
+	'ppsm'  => 'fa-file-powerpoint',
+	'sldx'  => 'fa-file-powerpoint',
+	'sldm'  => 'fa-file-powerpoint',
+	
+	// Other MS Office Documents
+	'pub'   => 'fa-file-powerpoint',
+	'xps'   => 'fa-print',
 
     // Executables
     'app'   => 'fa-list-alt',
-    'bat'   => 'fa-list-alt',
     'com'   => 'fa-list-alt',
     'exe'   => 'fa-list-alt',
     'jar'   => 'fa-list-alt',
@@ -84,6 +121,7 @@ return array(
     'psd'   => 'fa-picture-o',
     'tga'   => 'fa-picture-o',
     'tif'   => 'fa-picture-o',
+	'ico'   => 'fa-picture-o',
 
     // Package Files
     'box'   => 'fa-archive',
@@ -94,9 +132,14 @@ return array(
     'bat'   => 'fa-terminal',
     'cmd'   => 'fa-terminal',
     'sh'    => 'fa-terminal',
+	'vbs'   => 'fa-terminal',
+	'ps1'   => 'fa-terminal',
 
     // Text
     'cfg'   => 'fa-file-text',
+	'conf'  => 'fa-file-text',
+	'cnf'   => 'fa-file-text',
+	'config'=> 'fa-file-text',
     'ini'   => 'fa-file-text',
     'log'   => 'fa-file-text',
     'md'    => 'fa-file-text',
@@ -117,6 +160,7 @@ return array(
     'mov'   => 'fa-youtube-play',
     'mp4'   => 'fa-youtube-play',
     'mpg'   => 'fa-youtube-play',
+	'mpeg'  => 'fa-youtube-play',
     'ogv'   => 'fa-youtube-play',
     'webm'  => 'fa-youtube-play',
     'wmv'   => 'fa-youtube-play',

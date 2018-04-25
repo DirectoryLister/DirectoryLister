595e8644f7ca78335c751152bce47163fe316d24
OstenTV <info@ostentv.dk>
2018-04-25 22:39:10 +0200

    Updated Font Awesome

--- resources/themes/bootstrap/index.php
+++ resources/themes/bootstrap/index.php
@@ -9,7 +9,7 @@
 
         <!-- STYLES -->
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.0.10/css/all.css">
         <link rel="stylesheet" type="text/css" href="<?php echo THEMEPATH; ?>/css/style.css">
 
         <!-- SCRIPTS -->

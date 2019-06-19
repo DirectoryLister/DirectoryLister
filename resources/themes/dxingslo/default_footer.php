921c6207ad2932a22d868e240ba0edf0c709a39e
veso266 <veso266@gmail.com>
2019-06-19 21:52:53 +0200

    Added my theme

---
+++ resources/themes/dxingslo/default_footer.php
@@ -0,0 +1,5 @@
+<hr>
+
+<div class="footer">
+    <a href="http://www.dxing.si">DXing Slovenija</a> Tukaj se marsikaj dobi
+</div>
---
+++ resources/themes/dxingslo/js/directorylister.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+
+    // Get page-content original position
+    var contentTop = $('#page-content').offset().top;
+
+    // Show/hide top link on page load
+    showHideTopLink(contentTop);
+
+    // Show/hide top link on scroll
+    $(window).scroll(function() {
+        showHideTopLink(contentTop);
+    });
+
+    // Scroll page on click action
+    $('#page-top-link').click(function() {
+        $('html, body').animate({ scrollTop: 0 }, 'fast');
+        return false;
+    });
+
+    // Hash button on click action
+    $('.file-info-button').click(function(event) {
+
+        // Get the file name and path
+        var name = $(this).closest('li').attr('data-name');
+        var path = $(this).closest('li').attr('data-href');
+
+        // Set modal title value
+        $('#file-info-modal .modal-title').text(name);
+
+        $('#file-info .md5-hash').text('Loading...');
+        $('#file-info .sha1-hash').text('Loading...');
+        $('#file-info .filesize').text('Loading...');
+
+        $.ajax({
+            url:     '?hash=' + path,
+            type:    'get',
+            success: function(data) {
+
+                // Parse the JSON data
+                var obj = jQuery.parseJSON(data);
+
+                // Set modal pop-up hash values
+                $('#file-info .md5-hash').text(obj.md5);
+                $('#file-info .sha1-hash').text(obj.sha1);
+                $('#file-info .filesize').text(obj.size);
+
+            }
+        });
+
+        // Show the modal
+        $('#file-info-modal').modal('show');
+
+        // Prevent default link action
+        event.preventDefault();
+
+    });
+
+});
+
+function showHideTopLink(elTop) {
+    if($('#page-navbar').offset().top + $('#page-navbar').height() >= elTop) {
+        $('#page-top-nav').show();
+    } else {
+        $('#page-top-nav').hide();
+    }
+}

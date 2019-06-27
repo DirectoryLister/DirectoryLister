20d2663245f620e28f85ad5b8c9c7c10fbd1d1e2
veso266 <veso266@gmail.com>
2019-06-27 13:59:55 +0200

    Removed redundant code

--- resources/themes/bootstrap_dc/default_footer.php
+++
@@ -1,6 +0,0 @@
-<hr>
-
-<div class="footer">
-    Powered by, <a href="http://www.directorylister.com">Directory Lister</a>
-	<p>Total Downloads: <?php echo $lister->getTotalDownloads(); ?></p>
-</div>
--- resources/themes/dxingslo/default_footer.php
+++
@@ -1,6 +0,0 @@
-<hr>
-
-<div class="footer">
-    <a href="http://www.dxing.si">DXing Slovenija</a> Tukaj se marsikaj dobi
-	<p>Total Downloads: <?php echo $lister->getTotalDownloads(); ?></p>
-</div>

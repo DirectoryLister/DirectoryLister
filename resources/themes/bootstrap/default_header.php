9778d46fbd8ac44c04102c6f34e212aa43189c90
Vincent Sylvester <vincentsylvester@zetamex.com>
2019-06-27 14:25:34 +0200

    Fix up toggle, tabbed index.php

--- resources/themes/bootstrap/default_header.php
+++ resources/themes/bootstrap/default_header.php
@@ -1,10 +1,3 @@
 <div class="header">
-<div id="navbar-left"> <!-- Dark/Light Theme Toggle Switch --->
-	<ul id="page-top-nav" class="nav navbar-nav">
-		<li>
-			<input type="checkbox" id="switch" /><label for="switch">Toggle</label>
-		</li>
-	</ul>
-</div>
 <div class="text-center" id="description"></div>
 </div>
--- resources/themes/bootstrap/index.php
+++ resources/themes/bootstrap/index.php
@@ -2,182 +2,190 @@
 
 <html data-theme="light">
 
-    <head>
+	<head>
 
-        <title>Directory listing of <?php echo $lister->getListedPath(); ?></title>
-        <link rel="shortcut icon" href="<?php echo THEMEPATH; ?>/img/folder.png">
+		<title>Directory listing of <?php echo $lister->getListedPath(); ?></title>
+		<link rel="shortcut icon" href="<?php echo THEMEPATH; ?>/img/folder.png">
 
-        <!-- STYLES -->
-        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-        <link rel="stylesheet" type="text/css" href="<?php echo THEMEPATH; ?>/css/style.css">
+		<!-- STYLES -->
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo THEMEPATH; ?>/css/style.css">
 
-        <!-- SCRIPTS -->
-        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-        <script type="text/javascript" src="<?php echo THEMEPATH; ?>/js/directorylister.js"></script>
+		<!-- SCRIPTS -->
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="<?php echo THEMEPATH; ?>/js/directorylister.js"></script>
 
-        <!-- FONTS -->
-        <link rel="stylesheet" type="text/css"  href="//fonts.googleapis.com/css?family=Cutive+Mono">
+		<!-- FONTS -->
+		<link rel="stylesheet" type="text/css"  href="//fonts.googleapis.com/css?family=Cutive+Mono">
 
-        <!-- META -->
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta charset="utf-8">
+		<!-- META -->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta charset="utf-8">
 
-        <?php file_exists('analytics.inc') ? include('analytics.inc') : false; ?>
+		<?php file_exists('analytics.inc') ? include('analytics.inc') : false; ?>
 
-    </head>
+	</head>
 
-    <body>
+	<body>
 
-        <div id="page-navbar" class="navbar navbar-default navbar-fixed-top">
-            <div class="container">
+		<div id="page-navbar" class="navbar navbar-default navbar-fixed-top">
+			<div class="container">
 
-                <?php $breadcrumbs = $lister->listBreadcrumbs(); ?>
+				<?php $breadcrumbs = $lister->listBreadcrumbs(); ?>
 
-                <p class="navbar-text">
-                    <?php foreach($breadcrumbs as $breadcrumb): ?>
-                        <?php if ($breadcrumb != end($breadcrumbs)): ?>
-                                <a href="<?php echo $breadcrumb['link']; ?>"><?php echo $breadcrumb['text']; ?></a>
-                                <span class="divider">/</span>
-                        <?php else: ?>
-                            <?php echo $breadcrumb['text']; ?>
-                        <?php endif; ?>
-                    <?php endforeach; ?>
-                </p>
+				<p class="navbar-text">
+					<?php foreach($breadcrumbs as $breadcrumb): ?>
+						<?php if ($breadcrumb != end($breadcrumbs)): ?>
+								<a href="<?php echo $breadcrumb['link']; ?>"><?php echo $breadcrumb['text']; ?></a>
+								<span class="divider">/</span>
+						<?php else: ?>
+							<?php echo $breadcrumb['text']; ?>
+						<?php endif; ?>
+					<?php endforeach; ?>
+				</p>
+				
+				<div id="navbar-left"> <!-- Dark/Light Theme Toggle Switch --->
+					<ul id="page-top-nav" class="nav navbar-nav">
+						<li>
+							<input type="checkbox" id="switch" /><label for="switch">Toggle</label>
+						</li>
+					</ul>
+				</div>
 
-                <div class="navbar-right">
+				<div class="navbar-right">
 
-                    <ul id="page-top-nav" class="nav navbar-nav">
-                        <li>
-                            <a href="javascript:void(0)" id="page-top-link">
-                                <i class="fa fa-arrow-circle-up fa-lg"></i>
-                            </a>
-                        </li>
-                    </ul>
+					<ul id="page-top-nav" class="nav navbar-nav">
+						<li>
+							<a href="javascript:void(0)" id="page-top-link">
+								<i class="fa fa-arrow-circle-up fa-lg"></i>
+							</a>
+						</li>
+					</ul>
 
-                    <?php  if ($lister->isZipEnabled()): ?>
-                        <ul id="page-top-download-all" class="nav navbar-nav">
-                            <li>
-                                <a href="?zip=<?php echo $lister->getDirectoryPath(); ?>" id="download-all-link">
-                                    <i class="fa fa-download fa-lg"></i>
-                                </a>
-                            </li>
-                        </ul>
-                    <?php endif; ?>
-                </div>
+					<?php  if ($lister->isZipEnabled()): ?>
+						<ul id="page-top-download-all" class="nav navbar-nav">
+							<li>
+								<a href="?zip=<?php echo $lister->getDirectoryPath(); ?>" id="download-all-link">
+									<i class="fa fa-download fa-lg"></i>
+								</a>
+							</li>
+						</ul>
+					<?php endif; ?>
+				</div>
 
-            </div>
-        </div>
+			</div>
+		</div>
 
-        <div id="page-content" class="container">
+		<div id="page-content" class="container">
 
-            <?php file_exists('header.php') ? include('header.php') : include($lister->getThemePath(true) . "/default_header.php"); ?>
+			<?php file_exists('header.php') ? include('header.php') : include($lister->getThemePath(true) . "/default_header.php"); ?>
 
-            <?php if($lister->getSystemMessages()): ?>
-                <?php foreach ($lister->getSystemMessages() as $message): ?>
-                    <div class="alert alert-<?php echo $message['type']; ?>">
-                        <?php echo $message['text']; ?>
-                        <a class="close" data-dismiss="alert" href="#">&times;</a>
-                    </div>
-                <?php endforeach; ?>
-            <?php endif; ?>
+			<?php if($lister->getSystemMessages()): ?>
+				<?php foreach ($lister->getSystemMessages() as $message): ?>
+					<div class="alert alert-<?php echo $message['type']; ?>">
+						<?php echo $message['text']; ?>
+						<a class="close" data-dismiss="alert" href="#">&times;</a>
+					</div>
+				<?php endforeach; ?>
+			<?php endif; ?>
 
-            <?php 
-            $link = 'dir=.';
-            if(isset($_GET['dir'])) {
-                $link = 'dir='.$_GET['dir'];
-            }
-            $order = 'order=asc';
-            if(isset($_GET['order']) && $_GET['order'] === 'asc'){
-                $order = 'order=desc';
-            }
-            ?>
+			<?php 
+			$link = 'dir=.';
+			if(isset($_GET['dir'])) {
+				$link = 'dir='.$_GET['dir'];
+			}
+			$order = 'order=asc';
+			if(isset($_GET['order']) && $_GET['order'] === 'asc'){
+				$order = 'order=desc';
+			}
+			?>
 
-            <div id="directory-list-header">
-                <div class="row">
-                    <a class="col-md-7 col-sm-6 col-xs-10" href="<?php echo $lister->GetBasePath(); ?>?<?php echo $link . '&by=name&'. $order ?>">File</a>
-                    <a class="col-md-2 col-sm-2 col-xs-2 text-right" href="<?php echo $lister->GetBasePath(); ?>?<?php echo $link . '&by=size&'. $order ?>">Size</a>
-                    <a class="col-md-3 col-sm-4 hidden-xs text-right" href="<?php echo $lister->GetBasePath(); ?>?<?php echo $link . '&by=lastModified&'. $order ?>">Last Modified</a>
-                </div>
-            </div>
+			<div id="directory-list-header">
+				<div class="row">
+					<a class="col-md-7 col-sm-6 col-xs-10" href="<?php echo $lister->GetBasePath(); ?>?<?php echo $link . '&by=name&'. $order ?>">File</a>
+					<a class="col-md-2 col-sm-2 col-xs-2 text-right" href="<?php echo $lister->GetBasePath(); ?>?<?php echo $link . '&by=size&'. $order ?>">Size</a>
+					<a class="col-md-3 col-sm-4 hidden-xs text-right" href="<?php echo $lister->GetBasePath(); ?>?<?php echo $link . '&by=lastModified&'. $order ?>">Last Modified</a>
+				</div>
+			</div>
 
-            <ul id="directory-listing" class="nav nav-pills nav-stacked">
+			<ul id="directory-listing" class="nav nav-pills nav-stacked">
 
-                <?php foreach($dirArray as $name => $fileInfo): ?>
-                    <li data-name="<?php echo $name; ?>" data-href="<?php echo $fileInfo['url_path']; ?>">
+				<?php foreach($dirArray as $name => $fileInfo): ?>
+					<li data-name="<?php echo $name; ?>" data-href="<?php echo $fileInfo['url_path']; ?>">
 						<a href="<?php if(is_dir($fileInfo['file_path'])) { echo '?dir=' . $fileInfo['file_path']; } elseif($fileInfo['icon_class'] == 'fa-level-up') { echo  $fileInfo['file_path']; } else { echo 'download.php?file='. $fileInfo['file_path']; } ?>" class="clearfix" data-name="<?php echo $name; ?>">
 
-                            <div class="row">
-                                <span class="file-name col-md-7 col-sm-6 col-xs-9">
-                                    <i class="fa <?php echo $fileInfo['icon_class']; ?> fa-fw"></i>
-                                    <?php echo $name; ?>
-                                </span>
+							<div class="row">
+								<span class="file-name col-md-7 col-sm-6 col-xs-9">
+									<i class="fa <?php echo $fileInfo['icon_class']; ?> fa-fw"></i>
+									<?php echo $name; ?>
+								</span>
 
-                                <span class="file-size col-md-2 col-sm-2 col-xs-3 text-right">
-                                    <?php echo $fileInfo['file_size']; ?>
-                                </span>
+								<span class="file-size col-md-2 col-sm-2 col-xs-3 text-right">
+									<?php echo $fileInfo['file_size']; ?>
+								</span>
 
-                                <span class="file-modified col-md-3 col-sm-4 hidden-xs text-right">
-                                    <?php echo $fileInfo['mod_time']; ?>
-                                </span>
-                            </div>
+								<span class="file-modified col-md-3 col-sm-4 hidden-xs text-right">
+									<?php echo $fileInfo['mod_time']; ?>
+								</span>
+							</div>
 
-                        </a>
+						</a>
 
-                        <?php if (is_file($fileInfo['file_path'])): ?>
+						<?php if (is_file($fileInfo['file_path'])): ?>
 
-                            <a href="javascript:void(0)" class="file-info-button">
-                                <i class="fa fa-info-circle"></i>
-                            </a>
+							<a href="javascript:void(0)" class="file-info-button">
+								<i class="fa fa-info-circle"></i>
+							</a>
 
-                        <?php endif; ?>
+						<?php endif; ?>
 
-                    </li>
-                <?php endforeach; ?>
+					</li>
+				<?php endforeach; ?>
 
-            </ul>
-        </div>
+			</ul>
+		</div>
 
-        <?php file_exists('footer.php') ? include('footer.php') : include($lister->getThemePath(true) . "/default_footer.php"); ?>
+		<?php file_exists('footer.php') ? include('footer.php') : include($lister->getThemePath(true) . "/default_footer.php"); ?>
 
-        <div id="file-info-modal" class="modal fade">
-            <div class="modal-dialog">
-                <div class="modal-content">
+		<div id="file-info-modal" class="modal fade">
+			<div class="modal-dialog">
+				<div class="modal-content">
 
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal">&times;</button>
-                        <h4 class="modal-title">{{modal_header}}</h4>
-                    </div>
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal">&times;</button>
+						<h4 class="modal-title">{{modal_header}}</h4>
+					</div>
 
-                    <div class="modal-body">
+					<div class="modal-body">
 
-                        <table id="file-info" class="table table-bordered">
-                            <tbody>
+						<table id="file-info" class="table table-bordered">
+							<tbody>
 
-                                <tr>
-                                    <td class="table-title">MD5</td>
-                                    <td class="md5-hash">{{md5_sum}}</td>
-                                </tr>
-
-                                <tr>
-                                    <td class="table-title">SHA1</td>
-                                    <td class="sha1-hash">{{sha1_sum}}</td>
-                                </tr>
 								<tr>
-                                    <td class="table-title">File Downloads</td>
-                                    <td class="file-downloads">{{file_downloads}}</td>
-                                </tr>
+									<td class="table-title">MD5</td>
+									<td class="md5-hash">{{md5_sum}}</td>
+								</tr>
 
-                            </tbody>
-                        </table>
+								<tr>
+									<td class="table-title">SHA1</td>
+									<td class="sha1-hash">{{sha1_sum}}</td>
+								</tr>
+								<tr>
+									<td class="table-title">File Downloads</td>
+									<td class="file-downloads">{{file_downloads}}</td>
+								</tr>
 
-                    </div>
+							</tbody>
+						</table>
 
-                </div>
-            </div>
-        </div>
+					</div>
 
-    </body>
+				</div>
+			</div>
+		</div>
+
+	</body>
 
 </html>

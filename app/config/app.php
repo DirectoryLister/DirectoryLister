522f5826e7fbb60b994d5995a546b21b9ef5cdcf
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2019-11-04 22:08:06 -0700

    Updated config files

--- app/config/app.php
+++ app/config/app.php
@@ -1,18 +1,56 @@
 <?php
 
 return [
+    /**
+     * The root directory to list.
+     *
+     * Default value: '.'
+     */
+    'root' => '.',
+
+    /**
+     * Whether or not to list folders before regular files.
+     *
+     * Possible values: true, false
+     * Default value: true
+     */
     'list_folders_first' => true,
 
-    'list_sort_order' => 'natcasesort',
+    /**
+     * Sorting order of files.
+     *
+     * Possible values: foo, bar, baz
+     * Default value: natcasesort
+     */
+    'sort_order' => 'natcasesort',
 
+    /**
+     * Name of the theme to use for styling the application.
+     *
+     * Default value: default
+     */
     'theme' => 'default',
 
+    /**
+     * Description here...
+     *
+     * Default value: 'Y-m-d H:i:s'
+     */
     'date_format' => 'Y-m-d H:i:s',
 
+    /**
+     * Description here...
+     *
+     * Default value: []
+     */
     'hidden_files' => [
-        'some.file', // Files
-        'some_dir',  // Directories
-        '**/.ht*',   // Globs
-        '/^.ht*$/',  // Regular expressions
+        // ...
     ],
+
+    /**
+     * Hide version control system files (e.g. .git directories) from listing.
+     *
+     * Default value: true
+     */
+    'ignore_vcs_files' => true,
 ];
--- app/config/cache.php
+++ app/config/cache.php
@@ -1,5 +1,10 @@
 <?php
 
 return [
-    // ...
+    /**
+     * Path to the view cache directory.
+     *
+     * Default value: 'app/cache/views'
+     */
+    'view_cache' => env('VIEW_CACHE', 'app/cache/views'),
 ];

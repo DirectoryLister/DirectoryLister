2da38b019b99c7915fd95be5d636bc316893d4fe
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2019-12-30 23:30:09 -0700

    Added Twitter and GitHub links to footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
     theme: {
-        extend: {}
+        extend: {
+            textColor: {
+                github: '#171515',
+                twitter: '#1DA1F2'
+            }
+        }
     },
     variants: {
         visibility: ['responsive', 'hover', 'group-hover']

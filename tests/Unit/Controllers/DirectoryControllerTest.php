473d7b55e6b86359c994981eeab1446394475f98
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2020-01-05 17:43:26 -0700

    Added search request test

--- tests/Unit/Controllers/DirectoryControllerTest.php
+++ tests/Unit/Controllers/DirectoryControllerTest.php
@@ -5,6 +5,7 @@ namespace Tests\Controllers;
 use App\Bootstrap\ViewComposer;
 use App\Controllers\DirectoryController;
 use Psr\Http\Message\ResponseInterface;
+use Slim\Psr7\Request;
 use Slim\Psr7\Response;
 use Slim\Views\Twig;
 use Symfony\Component\Finder\Finder;
@@ -24,6 +25,7 @@ class DirectoryControllerTest extends TestCase
 
         $response = $controller(
             new Finder(),
+            $this->createMock(Request::class),
             new Response(),
             'tests/files'
         );
@@ -44,11 +46,38 @@ class DirectoryControllerTest extends TestCase
 
         $response = $controller(
             new Finder(),
+            $this->createMock(Request::class),
             new Response(),
             '404'
         );
 
         $this->assertInstanceOf(ResponseInterface::class, $response);
         $this->assertEquals(404, $response->getStatusCode());
+    }
+
+    public function test_it_returns_a_successful_response_for_a_search()
+    {
+        $this->container->call(ViewComposer::class);
+
+        $controller = new DirectoryController(
+            $this->container,
+            $this->config,
+            $this->container->get(Twig::class)
+        );
+
+        $request = $this->createMock(Request::class);
+        $request->method('getQueryParams')->willReturn([
+            'search' => 'charlie'
+        ]);
+
+        $response = $controller(
+            new Finder(),
+            $request,
+            new Response(),
+            'tests/files'
+        );
+
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+        $this->assertEquals(200, $response->getStatusCode());
     }
 }

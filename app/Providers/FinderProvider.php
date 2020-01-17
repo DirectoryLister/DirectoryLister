41595b2159c3659161654d89a6ea7a1af8dccc17
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2020-01-16 20:33:50 -0700

    Added 'mix-manifest.json' to the APP_FILES array

--- app/Providers/FinderProvider.php
+++ app/Providers/FinderProvider.php
@@ -14,7 +14,9 @@ use Tightenco\Collect\Support\Collection;
 class FinderProvider
 {
     /** @const Application paths to be hidden */
-    protected const APP_FILES = ['app', 'node_modules', 'vendor', 'index.php'];
+    protected const APP_FILES = [
+        'app', 'node_modules', 'vendor', 'mix-manifest.json', 'index.php'
+    ];
 
     /** @const Array of sort options mapped to their respective methods  */
     public const SORT_METHODS = [

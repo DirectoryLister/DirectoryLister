273e984cf1d31cf4cf43ae4ecd3a8a508a90e73f
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2020-02-15 23:07:23 -0700

    Changed temporary path used in TemporaryFileTest

--- tests/TemporaryFileTest.php
+++ tests/TemporaryFileTest.php
@@ -8,14 +8,14 @@ class TemporaryFileTest extends TestCase
 {
     public function test_it_can_create_a_temporary_file(): void
     {
-        $tempFile = new TemporaryFile(sys_get_temp_dir());
+        $tempFile = new TemporaryFile($this->filePath('app/cache'));
 
         $this->assertFileExists((string) $tempFile);
     }
 
     public function test_it_can_write_to_and_read_from_a_temporary_file(): void
     {
-        $tempFile = new TemporaryFile(sys_get_temp_dir());
+        $tempFile = new TemporaryFile($this->filePath('app/cache'));
 
         $this->assertFileIsReadable((string) $tempFile);
         $this->assertFileIsWritable((string) $tempFile);
@@ -27,7 +27,7 @@ class TemporaryFileTest extends TestCase
 
     public function test_it_removes_the_underlying_file_on_destruction(): void
     {
-        $tempFile = new TemporaryFile(sys_get_temp_dir());
+        $tempFile = new TemporaryFile($this->filePath('app/cache'));
         $filePath = (string) $tempFile;
 
         unset($tempFile);

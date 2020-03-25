76378f10a07347696cad83ae7af713e6962abe0b
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2020-03-25 15:40:23 -0700

    Added RouteManagerTest

---
+++ tests/Bootstrap/RouteManagerTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Bootstrap;
+
+use App\Bootstrap\RouteManager;
+use App\Controllers;
+use Slim\App;
+use Tests\TestCase;
+
+class RouteManagerTest extends TestCase
+{
+    public function test_it_registers_application_routes(): void
+    {
+        $app = $this->createMock(App::class);
+        $app->expects($this->once())->method('get')->with(
+            '/[{path:.*}]', Controllers\IndexController::class
+        );
+
+        (new RouteManager($app))();
+    }
+}

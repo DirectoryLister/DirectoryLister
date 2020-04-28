baa54bc517a205d980088cd2b34c171de881d62b
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2020-04-28 14:12:29 -0700

    Updated TranslationFactoryTest

--- tests/Factories/TranslationFactoryTest.php
+++ tests/Factories/TranslationFactoryTest.php
@@ -14,8 +14,17 @@ class TranslationFactoryTest extends TestCase
         $translator = (new TranslationFactory($this->container))();
 
         $this->assertEquals('en', $translator->getLocale());
+        $this->assertInstanceOf(MessageCatalogue::class, $translator->getCatalogue('de'));
         $this->assertInstanceOf(MessageCatalogue::class, $translator->getCatalogue('en'));
+        $this->assertInstanceOf(MessageCatalogue::class, $translator->getCatalogue('es'));
         $this->assertInstanceOf(MessageCatalogue::class, $translator->getCatalogue('fr'));
+        $this->assertInstanceOf(MessageCatalogue::class, $translator->getCatalogue('id'));
+        $this->assertInstanceOf(MessageCatalogue::class, $translator->getCatalogue('it'));
+        $this->assertInstanceOf(MessageCatalogue::class, $translator->getCatalogue('pl'));
+        $this->assertInstanceOf(MessageCatalogue::class, $translator->getCatalogue('ro'));
+        $this->assertInstanceOf(MessageCatalogue::class, $translator->getCatalogue('ru'));
+        $this->assertInstanceOf(MessageCatalogue::class, $translator->getCatalogue('zh-CN'));
+        $this->assertInstanceOf(MessageCatalogue::class, $translator->getCatalogue('zh-TW'));
     }
 
     public function test_it_throws_an_exception_for_an_invalid_language(): void

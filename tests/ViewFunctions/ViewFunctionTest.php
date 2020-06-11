9f2da572b9c674955b1f2b53e42ca9aea43e4000
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2020-06-11 09:08:21 -0700

    Added ViewFunctionTest

---
+++ tests/ViewFunctions/ViewFunctionTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\ViewFunctions;
+
+use App\ViewFunctions\ViewFunction;
+use Tests\TestCase;
+
+/** @covers \App\ViewFunctions\ViewFunction */
+class ViewFunctionTest extends TestCase
+{
+    public function test_it_can_be_extended(): void
+    {
+        $viewFunction = new class extends ViewFunction {
+            protected $name = 'foo';
+        };
+
+        $this->assertInstanceOf(ViewFunction::class, $viewFunction);
+        $this->assertEquals('foo', $viewFunction->name());
+    }
+}

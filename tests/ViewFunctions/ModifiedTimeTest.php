99304c6814b4a4dabd508df94948e8395dcd72ab
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2021-02-23 21:54:14 -0700

    Removed bad test

--- tests/ViewFunctions/ModifiedTimeTest.php
+++ tests/ViewFunctions/ModifiedTimeTest.php
@@ -3,7 +3,6 @@
 namespace Tests\ViewFunctions;
 
 use App\ViewFunctions\ModifiedTime;
-use RuntimeException;
 use Symfony\Component\Finder\SplFileInfo;
 use Tests\TestCase;
 
@@ -13,17 +12,6 @@ class ModifiedTimeTest extends TestCase
     {
         $file = $this->createMock(SplFileInfo::class);
         $file->method('getMTime')->willReturn(516976496);
-
-        $modifiedTime = new ModifiedTime($this->config);
-
-        $this->assertEquals('1986-05-20 12:34:56', $modifiedTime($file));
-    }
-
-    public function test_it_can_return_the_modified_time_for_a_symlink(): void
-    {
-        $file = $this->createMock(SplFileInfo::class);
-        $file->method('getMTime')->willThrowException(new RuntimeException);
-        $file->method('getPathname')->willReturn(dirname(__DIR__) . '/_files/somedir/broken.symlink');
 
         $modifiedTime = new ModifiedTime($this->config);
 

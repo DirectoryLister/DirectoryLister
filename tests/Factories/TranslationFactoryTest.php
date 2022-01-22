7600274a454a8d457b247e84cac69c9b1f14d50d
Albin Karlsson <albkar@student.chalmers.se>
2022-01-22 15:05:02 +0100

    Added Swedish translation

--- tests/Factories/TranslationFactoryTest.php
+++ tests/Factories/TranslationFactoryTest.php
@@ -31,6 +31,7 @@ class TranslationFactoryTest extends TestCase
         $this->assertInstanceOf(MessageCatalogue::class, $translator->getCatalogue('pt-BR'));
         $this->assertInstanceOf(MessageCatalogue::class, $translator->getCatalogue('ro'));
         $this->assertInstanceOf(MessageCatalogue::class, $translator->getCatalogue('ru'));
+        $this->assertInstanceOf(MessageCatalogue::class, $translator->getCatalogue('sv'));
         $this->assertInstanceOf(MessageCatalogue::class, $translator->getCatalogue('tr'));
         $this->assertInstanceOf(MessageCatalogue::class, $translator->getCatalogue('zh-CN'));
         $this->assertInstanceOf(MessageCatalogue::class, $translator->getCatalogue('zh-TW'));

155d18e4fbfbad08da5342935eaa77a38aaaddbe
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2022-01-27 09:12:19 -0700

    Updated Tailwind to v3.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,23 +3,16 @@ const colors = require('tailwindcss/colors');
 
 module.exports = {
     darkMode: 'class',
-    mode: 'jit',
-    purge: {
-        mode: 'all',
-        content: [
-            'app/resources/**/*.{js,scss}',
-            'app/src/**/*.php',
-            'app/views/**/*.twig',
-        ],
-        options: {
-            safelist: ['html', 'body', 'main', 'fab', 'fas', /^hljs/],
-        }
-    },
+    content: [
+        'app/resources/**/*.{js,scss}',
+        'app/src/**/*.php',
+        'app/views/**/*.twig',
+    ],
     theme: {
         extend: {
             colors: {
                 blue: colors.sky,
-                gray: colors.blueGray,
+                gray: colors.slate,
                 purple: colors.violet,
             },
             fontFamily: {
@@ -32,9 +25,5 @@ module.exports = {
                 twitter: '#1DA1F2'
             }
         }
-    },
-    variants: {
-        backgroundOpacity: ['dark'],
-        visibility: ['responsive', 'hover', 'group-hover']
     }
 };
